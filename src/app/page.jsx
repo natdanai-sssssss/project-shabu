6f374f76a5627e1696d0a475404e5d6446cf2f20
natdanai-sssssss <usergodbot02@gmail.com>
2024-10-14 14:22:47 +0700

    shabuji2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-import Link from "next/link";
 import Footer from "./components/Footer";
 import Nevbar from "./components/Nevbar";
-
 
 export default function Home() {
   return (
